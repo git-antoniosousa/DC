892f33377201bbe8a9b0b3201c7144907e4df40c
Kaixi26 <jorgejg575926@gmail.com>
2021-03-23 20:33:17 +0000

    inicio dos modelos

---
+++ addons/dissertation_admission_app/__manifest__.py
@@ -0,0 +1,8 @@
+{'name': 'Aplicação para Admissão a Dissertação',
+ 'description': 'Gestão do processo de admissão à dissertação',
+ 'depends': ['base'],
+ 'data': [
+ ],
+ 'application': True,
+ 'installable': True,
+ }
---
+++ addons/dissertation_admission_app/models/dissertation.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+from odoo.exceptions import Warning
+
+class Dissertation(models.Model):
+    _name = 'dissertation_admition.dissertation'
+    _description = 'Dissertação'
+
+    name = fields.Char('Titulo', required=True)
+    description = fields.Text('Descrição', required=True)
+    public = fields.Boolean('Ativo', default=False)
+    date_published = fields.Date('Data de criação')
+    publisher_id = fields.Many2one('res.partner', string='Autor')
+    adviser_id = fields.Many2one('res.partner', string='Orientador')
+    coadviser_id = fields.Many2one('res.partner', string='Coorientador')
+    student_id = fields.Many2one('res.partner', string='Estudante')
+    
