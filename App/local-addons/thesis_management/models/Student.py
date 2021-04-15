178f9c02b2d7ce055dd909cbf1472f0c2ed0a99e
Joel Ferreira <joel_alexandre98@hotmail.com>
2021-04-15 17:12:01 +0100

    removed last module

--- App/local-addons/thesis_management/models/Student.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, models, fields
-
-
-class Student(models.Model):
-    _name = "student"
-    student_num = fields.Char(string="Número Mecanográfico", required=True)
-    name = fields.Char(string="Nome", required=True, translate=True)
-    genre = fields.Selection([('m', 'Masculino'), ('f', 'Feminino'), ('o', 'Outro')], required=True)
-
