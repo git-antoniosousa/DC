f382f5781ad3c1c9be816392d32c2e3bf38be360
ruiAzevedo <ruiazevedo@MBP-de-Rui.lan>
2021-04-15 15:50:15 +0100

    something

--- App/local-addons/thesis_management/models/process.py
+++
@@ -1,5 +0,0 @@
-from odoo import api, models, fields
-
-
-class Process(models.Model):
-    pass
--- App/local-addons/thesis_management/models/student.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, models, fields
-
-
-class Student(models.Model):
-    _name = "student"
-    student_num = fields.Char(string="Student Number", required=True)
-    name = fields.Char(string="Student Name", required=True, translate=True)
-    genre = fields.Selection([('m', 'male'), ('f', 'female')], required=True)
-
