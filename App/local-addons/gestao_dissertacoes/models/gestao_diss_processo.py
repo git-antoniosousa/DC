1af7d7cdf60eff3f70c2a90ed81191bbdcc393b2
Joel Ferreira <joel_alexandre98@hotmail.com>
2021-04-22 23:45:15 +0100

    stage improvements

--- App/local-addons/gestao_dissertacoes/models/gestao_diss_processo.py
+++ App/local-addons/gestao_dissertacoes/models/gestao_diss_processo.py
@@ -6,11 +6,6 @@ class Processo(models.Model):
     _description = 'Processo de gestão da dissertação'
     _rec_name = 'aluno_id'
 
-    @api.model
-    def _default_processo_stage(self):
-        Estado = self.env['gest_diss.processo.estado']
-        return Estado.search([], limit=1)
-
     dissertacao_id = fields.Many2one('gest_diss.dissertacao', 'Dissertação')
 
     aluno_id = fields.Many2one('gest_diss.aluno', "Aluno")
@@ -27,26 +22,6 @@ class Processo(models.Model):
     nota = fields.Integer(string="Nota")
 
     data_homologacao = fields.Date(string="Data de Homologação")
-    estado_id = fields.Many2one(
-        'gest_diss.processo.estado',
-        default=_default_processo_stage
-    )
-
-    defesa_id = fields.Many2one('gest_diss.defesa', "Defesa")
-
-    def write(self, vals):
-        processo = super(Processo, self).write(vals)
-        if self.estado_id.estado:
-            self.estado_id.estado = self.estado_id.estado
-        return processo
-
-class ProcessoEstado(models.Model):
-    _name = 'gest_diss.processo.estado'
-    _order = 'sequence,name'
-
-    name = fields.Char()
-    sequence = fields.Integer()
-    fold = fields.Boolean()
     estado = fields.Selection([
         ('registo_inicial', 'Registo Inicial'),
         ('correcoes', 'Correções'),
@@ -61,3 +36,11 @@ class ProcessoEstado(models.Model):
         ('finalizado', 'Finalizado')
 
     ], 'Estado', default="registado")
+
+    defesa_id = fields.Many2one('gest_diss.defesa', "Defesa")
+
+    def write(self, vals):
+        processo = super(Processo, self).write(vals)
+        if self.estado_id.estado:
+            self.estado_id.estado = self.estado_id.estado
+        return processo
