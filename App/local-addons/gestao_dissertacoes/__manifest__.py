26a063bb062f4df425d26e2a30c6116f6e9ed769
JoaoLinhares <joaolinhares14@gmail.com>
2021-05-31 18:45:19 +0100

    Adição Categorias, adição links nos websites, correção get_name no aluno

--- App/local-addons/gestao_dissertacoes/__manifest__.py
+++ App/local-addons/gestao_dissertacoes/__manifest__.py
@@ -15,6 +15,7 @@
              'views/gestao_diss_membro.xml',
              'views/gestao_diss_filiacao.xml',
              'views/gestao_diss_curso.xml',
+             'views/gestao_diss_categoria.xml',
              'views/gestao_diss_dashboard.xml'
              ],
     'demo': [
