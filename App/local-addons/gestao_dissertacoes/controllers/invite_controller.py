8fd824b84feed75808648c083db12d0cce100e0e
JoaoLinhares <joaolinhares14@gmail.com>
2021-06-20 17:48:47 +0100

    ICS - form de dissertacao corrigido

--- App/local-addons/gestao_dissertacoes/controllers/invite_controller.py
+++ App/local-addons/gestao_dissertacoes/controllers/invite_controller.py
@@ -1,5 +1,7 @@
 from odoo import http
 from cryptography.fernet import Fernet, InvalidToken
+from datetime import datetime, timedelta
+import pytz
 
 class Invite(http.Controller):
     @http.route('/invite/<string:token>', auth='public')
@@ -20,6 +22,12 @@ class Invite(http.Controller):
         processo = http.request.env['gest_diss.processo'].sudo().search([('id', 'ilike', id)]) 
         if processo:
             processo_resposta = processo
+            local = pytz.timezone("Europe/Lisbon")
+            data = datetime.strftime(pytz.utc.localize(datetime.strptime(str(processo.data_hora), "%Y-%m-%d %H:%M:%S")).astimezone(local),"%d/%m/%Y %H:%M %Z%z")
+            data_inicio = datetime.strftime(pytz.utc.localize(datetime.strptime(str(processo.data_hora), "%Y-%m-%d %H:%M:%S")).astimezone(local),"%d-%m-%Y %H:%M")
+            data_fim = datetime.strftime(pytz.utc.localize(datetime.strptime(str(processo.data_hora + timedelta(minutes=55)), "%Y-%m-%d %H:%M:%S")).astimezone(local),"%d-%m-%Y %H:%M")
+            print(data_inicio)
+            print(data_fim)
             if(kw.get('convite')):
                 http.request.env['gest_diss.processo'].sudo().search([('id', 'ilike', id)]).convite(kw.get('convite'),juri)
                 processo_resposta = http.request.env['gest_diss.processo'].sudo().search([('id', 'ilike', id)])
@@ -27,16 +35,16 @@ class Invite(http.Controller):
                 if processo.juri_presidente_id.name != params[2]: 
                     return http.request.render('gestao_dissertacoes.not-found', {'code': 403 ,'msg': "Não tem acesso a este processo"})
                 else : 
-                    return http.request.render('gestao_dissertacoes.processo', {'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_presidente, 'existe': processo_resposta.juri_presidente_id})
+                    return http.request.render('gestao_dissertacoes.processo', {'data_inicio': data_inicio,'data_fim': data_fim,'data': data,'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_presidente, 'existe': processo_resposta.juri_presidente_id})
             if(juri == 'v'):
                 if processo.juri_vogal_id.name != params[2]: 
                     return http.request.render('gestao_dissertacoes.not-found', {'code': 403 ,'msg': "Não tem acesso a este processo"})
                 else:
-                    return http.request.render('gestao_dissertacoes.processo', {'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_vogal, 'existe': processo_resposta.juri_vogal_id})
+                    return http.request.render('gestao_dissertacoes.processo', {'data_inicio': data_inicio,'data_fim': data_fim,'data': data,'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_vogal, 'existe': processo_resposta.juri_vogal_id})
             if(juri == 'a'):
                 if processo.juri_arguente_id.name != params[2]: 
                     return http.request.render('gestao_dissertacoes.not-found', {'code': 403 ,'msg': "Não tem acesso a este processo"})
                 else:
-                    return http.request.render('gestao_dissertacoes.processo', {'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_arguente, 'existe': processo_resposta.juri_arguente_id})
+                    return http.request.render('gestao_dissertacoes.processo', {'data_inicio': data_inicio,'data_dim': data_fim,'data': data,'invite_processo': processo_resposta, 'tipo_juri': juri, 'status': processo_resposta.convite_arguente, 'existe': processo_resposta.juri_arguente_id})
         else:
             return http.request.render('gestao_dissertacoes.not-found', {'code': 404 ,'msg': "Processo não encontrado"})
